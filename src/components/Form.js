7aed335ca0194eeda6cb06ee83bca8293c43c002
Erickcamp <thecampkid@gmail.com>
2020-05-29 10:41:47 -0600

    step 1 done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { Component } from "react";
 
-function Form() {
-    return <div>Form</div>
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+        name: '',
+        price: 0,
+        imgurl: '',
+    };
+  }
+
+  handleInput = (e) => this.setState({[e.target.name]: e.target.value})
+  resetState = () => this.setState({name:'', price:0, imgurl:''})
+
+  render() {
+      const {name, price, imgurl} = this.state
+    return (
+      <div>
+        <div>Form</div>
+        <input
+        placeholder='Image URL'
+        name='imgurl'
+        value={imgurl}
+        onChange={this.handleInput} 
+        />
+        <input 
+        placeholder='Product Name'
+        name='name'
+        value={name}
+        onChange={this.handleInput}
+        />
+        <input 
+        placeholder='0'
+        name='price'
+        value={price}
+        onChange={this.handleInput} 
+        />
+        <button onClick={this.resetState}>Cancel</button>
+        <button>Add to Inventory</button>
+      </div>
+    );
+  }
 }
 
-export default Form
+export default Form;
